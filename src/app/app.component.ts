8f277fe9e53ee2a1e193805810ed01d551224485
aldrin2002 <fechalinaldrin1000@gmail.com>
2024-12-16 17:11:09 +0800

    file upload
    file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None // This will make styles global
 })
-export class AppComponent {
-  title = 'recipe-finder';
+export class AppComponent implements AfterViewInit {
+  title = 'food-hub';
+
+  ngAfterViewInit() {
+    const scrollableElement = document.querySelector('.scrollable') as HTMLElement;
+
+    // Show scrollbar when scrolling
+    if (scrollableElement) {
+      scrollableElement.addEventListener('scroll', () => {
+        scrollableElement.classList.add('show-scrollbar');
+
+        // Remove the class after a short delay to hide the scrollbar
+        clearTimeout((scrollableElement as any).scrollTimeout);
+        (scrollableElement as any).scrollTimeout = setTimeout(() => {
+          scrollableElement.classList.remove('show-scrollbar');
+        }, 100); // Adjust delay as needed
+      });
+    }
+  }
 }
--- src/app/parts/about/about.component.ts
+++ src/app/parts/about/about.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss'] // Use styleUrls instead of styleUrl
 })
-export class AboutComponent {
-
-}
+export class AboutComponent { }
